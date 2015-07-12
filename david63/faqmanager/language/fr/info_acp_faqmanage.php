cebc5287c0057a11e44c22a8e875e0adea389b6b
Raphaël M. <Galixte@users.noreply.github.com>
2015-07-12 17:31:10 +0200

    Update of FRENCH translation for FAQ manager extension

--- david63/faqmanager/language/fr/info_acp_faqmanage.php
+++ david63/faqmanager/language/fr/info_acp_faqmanage.php
@@ -40,8 +40,9 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'BACKUP_LOCATION_NO_WRITE'	=> 'Impossible de créer un fichier de sauvegarde. Merci de vérifier les permissions en écriture pour le répertoire store/faq_backup/ ainsi que les répertoires et fichiers se trouvant à l’intérieur.',
-	'BAD_FAQ_FILE'				=> 'Le fichier actuellement ouvert n’est pas un fichier de la FAQ.',
+	'BAD_FAQ_FILE'				=> 'Ce fichier n’est pas un fichier de la FAQ.',
 
+	'CATEGORY_NOT_EXIST'			=> 'La catégorie n’existe pas.',
 	'CREATE_CATEGORY'			=> 'Créer une catégorie',
 	'CREATE_FIELD'				=> 'Créer une question',
 
@@ -49,10 +50,10 @@ $lang = array_merge($lang, array(
 	'DELETE_VAR'				=> 'Supprimer une question',
 
 	'FAQ_ANSWER'				=> 'Réponse',
-	'FAQ_CAT_CAT'				=> 'Depuis cette page, il est possible de modifier les catégories.',
-	'FAQ_CAT_EDIT'				=> 'Depuis cette page, il est possible de modifier les questions.',
+	'FAQ_CAT_CAT'				=> 'Depuis cette page, il est possible de modifier la catégorie.',
+	'FAQ_CAT_EDIT'				=> 'Depuis cette page, il est possible de modifier la question.',
 	'FAQ_CAT_FAQ'				=> 'Depuis cette page, il est possible d’ajouter des questions pour cette catégorie.',
-	'FAQ_CAT_FILES'				=> 'Sélectionner le fichier de la FAQ à modifier.',
+	'FAQ_CAT_FILES'				=> 'Depuis cette page il est possible de modifier les fichiers de la FAQ.',
 	'FAQ_CAT_LIST'				=> 'Depuis cette page, il est possible de gérer les catégories du fichier sélectionné.<br /><br /><strong>Une catégorie nommée « -- » permet de créer un saut de ligne sur la page de la FAQ.</strong>',
 	'FAQ_CAT_SUCCESS'			=> 'La catégorie a été ajoutée avec succès.',
 	'FAQ_CAT_QUESTION'			=> 'Depuis cette page, il est possible de gérer les questions de la catégorie sélectionnée.',
@@ -60,7 +61,7 @@ $lang = array_merge($lang, array(
 	'FAQ_DELETE_SUCCESS'		=> 'Les données de la FAQ ont été supprimées avec succès.',
 	'FAQ_EDIT_SUCCESS'			=> 'La FAQ a été mise à jour avec succès.',
 	'FAQ_TOPIC_SUCCESS'			=> 'La question a été ajoutée avec succès.',
-	'FAQ_FILE_NOT_EXIST'		=> 'Le fichier en cours de modification n’existe pas.',
+	'FAQ_FILE_NOT_EXIST'		=> 'Ce fichier n’existe pas.',
 	'FAQ_FILE_NO_WRITE'			=> 'Impossible de mettre à jour le fichier.  Merci de vérifier les permissions en écriture pour ce fichier.',
 	'FAQ_FILE_SELECT'			=> 'Sélectionner un fichier de la FAQ à modifier.',
 	'FAQ_LANGUAGE'				=> 'Langue',
@@ -69,15 +70,15 @@ $lang = array_merge($lang, array(
 	'FAQ_MANAGE'				=> 'Gérer la FAQ',
 	'FAQ_NAME'					=> 'Nom',
 	'FAQ_QUESTION'				=> 'Question',
-	'FAQ_CAT_QUESTION'			=> 'Catégorie de questions',
+	'FAQ_CAT_SETTINGS_QUESTION'			=> 'Catégorie de questions',
 	'FAQ_SETTINGS'				=> 'Ajouter/modifier une question',
 	'FAQ_CAT_SETTINGS'			=> 'Modifier une catégorie de questions',
 
 	'LOAD_BACKUP'				=> 'Restaurer la sauvegarde',
 
-	'LOG_FAQ_ADD'				=> '<strong>L’article de la FAQ a été ajouté</strong>',
-	'LOG_FAQ_DELETE'			=> '<strong>L’article de la FAQ a été supprimé</strong>',
-	'LOG_FAQ_EDIT'				=> '<strong>L’article de la FAQ a été modifié</strong>',
+	'LOG_FAQ_ADD'				=> '<strong>Un article de la FAQ a été ajouté</strong>',
+	'LOG_FAQ_DELETE'			=> '<strong>Un article de la FAQ a été supprimé</strong>',
+	'LOG_FAQ_EDIT'				=> '<strong>Un article de la FAQ a été modifié</strong>',
 	'LOG_FAQ_RESTORE'			=> '<strong>LA FAQ a été restaurée depuis la sauvegarde</strong>',
 
 	'NOT_ALLOWED_OUT_OF_DIR'	=> 'Vous n’êtes pas autorisé à modifier les fichiers en dehors du répertoire de la langue.',
